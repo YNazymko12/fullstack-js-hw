5e9d277e2e7aad4554a5f4f95a05dabec2269d03
YNazymko12 <yulychka12@gmail.com>
2024-06-30 14:09:24 +0200

    added comments for hw-01

--- hw-01/js/task-2.js
+++ hw-01/js/task-2.js
@@ -1,3 +1,14 @@
+/*
+Оголоси функцію getShippingMessage, яка очікує три параметри, значення яких будуть задаватися під час її виклику: 
+• country — перший параметр, рядок, що містить країну доставки 
+• price — другий параметр, число, що містить загальну вартість товару 
+• deliveryFee — третій параметр, число, що містить вартість доставки товару
+
+Доповни код функції так, щоб вона повертала рядок з повідомленням про доставку товару в країну користувача: 
+"Shipping to <country> will cost <totalPrice> credits", де: 
+• <country> — це країни доставки 
+• <totalPrice> — це загальна вартість замовлення, що включає вартість товару і його доставки */
+
 function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
   return `Shipping to ${country} will cost ${totalPrice} credits"`;
